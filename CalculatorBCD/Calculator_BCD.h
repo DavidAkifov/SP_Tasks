9d51be3c092503566c498c503b42079cad8e2a82
DavidAkifov <deiv3639@gmail.com>
2024-03-30 23:28:19 +0200

    adding operation plus and minus
    operation plus is working
    operation minus is not fully implemented
    
    rework on some functions is mandatory.

--- CalculatorBCD/Calculator_BCD.h
+++ CalculatorBCD/Calculator_BCD.h
@@ -1,10 +1,14 @@
 #ifndef CALCULATOR_BCD_H
 #define CALCULATOR_BCD_H
 #include <stdio.h>
+#include <stdint.h>
 
-unsigned int convertToBCD(int dec_value);
-unsigned int appendIntegers(unsigned int num1, unsigned int num2);
-unsigned int digitToBinary(int digit, unsigned int result, int iteration);
-unsigned int binaryStringToInt(const char* binaryStr);
+uint64_t convert_to_BCD(int dec_value);
+uint64_t sum_BCD(uint64_t first_num, uint64_t second_num);
+uint64_t subtract_BCD(uint64_t first_num, uint64_t second_num);
+uint64_t digitToBinary(int digit, uint64_t result, int iteration);
+int correct_for_subtraction(int number);
+int findIntLength(int num);
+int getMSB(uint64_t num);
 
 #endif /* CONVERTER_H */
