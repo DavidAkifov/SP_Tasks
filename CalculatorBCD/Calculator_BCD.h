a9a52b543c0988a3415a06ba9f563dcad575181d
DavidAkifov <deiv3639@gmail.com>
2024-03-31 14:13:48 +0300

    stable state

--- CalculatorBCD/Calculator_BCD.h
+++ CalculatorBCD/Calculator_BCD.h
@@ -4,11 +4,12 @@
 #include <stdint.h>
 
 uint64_t convert_to_BCD(int dec_value);
-uint64_t sum_BCD(uint64_t first_num, uint64_t second_num);
-uint64_t subtract_BCD(uint64_t first_num, uint64_t second_num);
+uint64_t sum_BCD(int first_number, int second_number);
+uint64_t prep_for_subtract(int number);
 uint64_t digitToBinary(int digit, uint64_t result, int iteration);
-int correct_for_subtraction(int number);
+void correct_for_subtraction(int *number);
 int findIntLength(int num);
 int getMSB(uint64_t num);
+void print_bcd_in_binary(uint64_t number);
 
 #endif /* CONVERTER_H */
