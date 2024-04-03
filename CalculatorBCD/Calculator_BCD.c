383e0538a26a67a1c47923f39c917e3707ba2845
DavidAkifov <deiv3639@gmail.com>
2024-04-03 20:03:15 +0300

    fixup: added comments

--- CalculatorBCD/Calculator_BCD.c
+++ CalculatorBCD/Calculator_BCD.c
@@ -9,70 +9,79 @@ int main()
 {
     while(1)
     {
-    printf("-------------------------------------------------------------\n");
-    int first_number, second_number;
-    char operation_simbol;  
-    printf("Enter your first number: ");
-    scanf("%d", &first_number);  
-    printf("Enter your second number: ");
-    scanf("%d", &second_number);  
-    printf("Enter Operation (+/-): ");
-    fflush(stdin);  
-    scanf("%c", &operation_simbol);
-    getchar();  
-    if(operation_simbol == '+')   
-    {
-        uint64_t result = sum_bcd(convert_to_bcd(first_number),convert_to_bcd(second_number));  
-        printf("Result in BCD(binary): ");
-        print_bcd_in_binary(result, 0); 
-        printf("\n");
-    } 
-    else if (operation_simbol == '-')  
-    {
-        uint64_t result = subtract(first_number, second_number);  
-        printf("Result in BCD(binary): ");
-        if(first_number<second_number)  
+        printf("-------------------------------------------------------------\n");
+        int first_number, second_number;
+        char operation_simbol;  
+        printf("Enter your first number: ");
+        scanf("%d", &first_number);  
+        printf("Enter your second number: ");
+        scanf("%d", &second_number);  
+        printf("\nFirst Number:              ");
+        print_bcd_in_binary(convert_to_bcd(first_number),0);
+        printf("\nSecond Number:             ");
+        print_bcd_in_binary(convert_to_bcd(second_number),0);
+        printf("Enter Operation (+/-): ");
+        fflush(stdin);  
+        scanf("%c", &operation_simbol);
+        getchar();  
+        if(operation_simbol == '+')   
         {
-            print_bcd_in_binary(result,1);
+            uint64_t result = sum_bcd(convert_to_bcd(first_number),convert_to_bcd(second_number));  
+            printf("Result in BCD(binary):     ");
+            print_bcd_in_binary(result, 0); 
+            printf("\n");
+        } 
+        else if (operation_simbol == '-')  
+        {
+            uint64_t result = subtract(first_number, second_number);  
+            printf("Result in BCD(binary):     ");
+            if(first_number<second_number)  
+            {
+                print_bcd_in_binary(result,1);
+            }
+            else
+                print_bcd_in_binary(result,0);  
+        } 
+        else{
+            printf("[main]: unsuported operation!\n");
         }
-        else
-            print_bcd_in_binary(result,0);  
-    } 
-    else{
-        printf("[main]: unsuported operation!\n");
-    }
-    char again;
-    printf("Do you want to calculate again? (y/n): ");  
-    fflush(stdin);
-    scanf("%c", &again);
-    getchar();
-    if(again == 'n')
-    {
-        break;
-    }
+        char again;
+        printf("\nDo you want to calculate again? (y/n): ");  
+        fflush(stdin);
+        scanf("%c", &again);
+        getchar();
+        if(again == 'n')
+        {
+            break;
+        }
     }
 
     return 0;
 }
 
-uint64_t sum_bcd(uint64_t first_number, uint64_t second_number) {
+uint64_t sum_bcd(uint64_t first_number, uint64_t second_number) 
+{
     uint64_t result = 0;
     uint64_t carry = 0;
     const uint64_t mask = 0x0F;
     
 
      
-    for (int i = 0; i < 63; i += 4) {
+    for (int i = 0; i < 63; i += 4) 
+    {
         uint64_t digit1 = (first_number >> i) & mask;  
         uint64_t digit2 = (second_number >> i) & mask;  
         
         uint64_t sum = digit1 + digit2 + carry;  
         
          
-        if (sum > 9) { 
+        if (sum > 9) 
+        { 
             carry = 1;  
             sum -= 10;  
-        } else {
+        } 
+        else 
+        {
             carry = 0;
         }
         
@@ -97,10 +106,12 @@ uint64_t convert_to_bcd(int dec_value)
     return valueToBCD;
 }
 
-int findIntLength(int num) {  
+int find_int_lenght(int num) 
+{  
     int length = 0;
 
-    if (num < 0) {  
+    if (num < 0) 
+    {  
         return 0;
     }
 
@@ -109,10 +120,13 @@ int findIntLength(int num) {
 }
 
 
-int get_msb(uint64_t num) {
+int get_msb(uint64_t num) 
+{
      
-    for (int msb_position = 63; msb_position >= 0; msb_position--) {
-        if ((num >> msb_position) & 1) {
+    for (int msb_position = 63; msb_position >= 0; msb_position--) 
+    {
+        if ((num >> msb_position) & 1) 
+        {
             return msb_position;
         }
     }
@@ -127,12 +141,15 @@ void print_bcd_in_binary(uint64_t number, uint8_t flag)
         printf("0000 ");
     }
      
-    for (int i = 60; i >= 0; i -= 4) {
+    for (int i = 60; i >= 0; i -= 4) 
+    {
         uint64_t bit = (number >> i) & 0xF;  
-        if (bit > 0 || first_one_found || i == 0) {  
-            printf("%s ", decimal_to_binary_4_bits(bit));
+        if (bit > 0 || first_one_found || i == 0) 
+        {  
+            printf("%s", decimal_to_binary_4_bits(bit));
             first_one_found = 1;  
-            if ((i % 4 == 0) && (i != 0)) {
+            if ((i % 4 == 0) && (i != 0)) 
+            {
                 printf(" ");  
             }
         }
@@ -140,7 +157,8 @@ void print_bcd_in_binary(uint64_t number, uint8_t flag)
     printf("\n");
 }
 
-const char* decimal_to_binary_4_bits(unsigned int num) {
+const char* decimal_to_binary_4_bits(unsigned int num) 
+{
      
     static const char* binaryLookup[10] = {
         "0000", "0001", "0010", "0011", "0100",
@@ -151,15 +169,18 @@ const char* decimal_to_binary_4_bits(unsigned int num) {
 }
 
  
-int int_append(int to_append, uint16_t append) {
+int int_append(int to_append, uint16_t append) 
+{
     to_append = to_append * 10 + append;
     return to_append;
 }
  
-void add_nine_for_sign(uint64_t *number) {
+void add_nine_for_sign(uint64_t *number) 
+{
     int number_length = get_msb(*number);
     
-    for (int i = 0; i < 64; i += 4) {
+    for (int i = 0; i < 64; i += 4) 
+    {
         if (i > number_length) {
             *number |= 9ULL << i;  
             break;
@@ -169,16 +190,22 @@ void add_nine_for_sign(uint64_t *number) {
 
 
  
-uint64_t subtract(int first_number, int second_number) {
-    if (!second_number) {
+uint64_t subtract(int first_number, int second_number) 
+{
+    if (!second_number) 
+    {
         return sum_bcd(convert_to_bcd(first_number), convert_to_bcd(second_number));  
-    } else if (!first_number) {  
+    } 
+    else if (!first_number) 
+    {  
         uint64_t result = convert_to_bcd(second_number);  
         add_nine_for_sign(&result);  
         return result;  
-    } else {
+    } 
+    else 
+    {
         uint8_t result_flag = (first_number > second_number) ? 0 : 1;  
-        int length = findIntLength(second_number);  
+        int length = find_int_lenght(second_number);  
         second_number = (int)pow(10, length)  - second_number;  
         uint64_t second_number_bcd = convert_to_bcd(second_number); 
         add_nine_for_sign(&second_number_bcd); 
@@ -190,19 +217,22 @@ uint64_t subtract(int first_number, int second_number) {
         int decimal_value = 0;
         for (int i = 60; i >= 0; i -= 4) {
             uint64_t bit = (result_bcd >> i) & 0xF;
-            if (bit > 0 || first_one_found || i == 0) {
-                if (!first_one_found) {
+            if (bit > 0 || first_one_found || i == 0) 
+            {
+                if (!first_one_found) 
+                {
                     uint64_t mask = ~(0xFULL << i);
                     result_bcd &= mask;
                     first_one_found = 1;
                     continue;
-                }            
+                }
                 decimal_value = int_append(decimal_value, bit);
             }
         }
          
-        if (result_flag && decimal_value) {
-            int tens_complement = pow(10, findIntLength(decimal_value)) - decimal_value;
+        if (result_flag && decimal_value) 
+        {
+            int tens_complement = pow(10, find_int_lenght(decimal_value)) - decimal_value;
             result_bcd = convert_to_bcd(tens_complement);  
             add_nine_for_sign(&result_bcd);  
         }
